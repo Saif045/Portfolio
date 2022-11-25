91c493ed856e108a4456b679f5ed6dec78124463
Saif045 <saif.x45@gmail.com>
2022-11-25 12:15:25 +0200

    sfaaa

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,13 @@
+import anubis from "./anubis.webp";
+
+export { anubis };
+
+/* 
 import hollowknight from "./hollowknight.svg";
 import smalldeer from "./smalldeer.svg";
 import s from "./s.png";
 import ss from "./ss.svg";
 import me from "./me.png";
-import anubis from "./anubis.webp";
+{ hollowknight, smalldeer, s, ss ,me ,anubis};
 
-
-
-export { hollowknight, smalldeer, s, ss ,me ,anubis};
+*/
