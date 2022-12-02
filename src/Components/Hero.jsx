8a8a923aaf43a5b9355197066e0d56958cc50c06
Saif045 <saif.x45@gmail.com>
2022-12-02 17:48:36 +0200

    gas

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
   };
 
   return (
-    <div className="  flex flex-col sm:flex-row  justify-between h-screen ">
+    <div className="  flex flex-col sm:flex-row  sm:justify-between h-screen ">
       <Bg />
 
       <motion.div
