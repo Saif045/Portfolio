a0af4affa8bae53dbabab3e4008d9736e8c1b74a
Saif045 <saif.x45@gmail.com>
2022-12-02 17:58:26 +0200

    kimk

--- src/Components/utils/Bg.tsx
+++ src/Components/utils/Bg.tsx
@@ -77,7 +77,7 @@ const Bg = () => {
       mouseMoveForce={motionForce}
       touchMoveForce={motionForce}
       backgroundColor="ffffff00"
-      className="  pb-20 sm:p-0 m-0 z-50 "
+      className="  pb-28 sm:p-0 m-0 z-50 "
     />
   );
 };
