0a065885dee02550479ae620b8917d4b8c57ed39
Saif045 <saif.x45@gmail.com>
2022-12-12 15:54:13 +0200

    gdf

--- src/Components/pages/skills/Skills.jsx
+++ src/Components/pages/skills/Skills.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import { skills, list } from "../../utils/skills";
 import { motion } from "framer-motion";
 
+const item = {
+  show: {
+    x: 0,
+    opacity: 1,
+    transition: { type: "easeIn", duration: 0.8 },
+  },
+};
+
 const Skills = () => {
   return (
     <div className=" sm:w-[calc(100vw-5rem)] sm:absolute sm:right-0 pt-14  ">
       <div className="container mx-auto px-2 grid h-[80vh]  ">
-        <motion.div className="sm:w-1/2 lg:w-1/3 m-4 self-center  ">
+        <motion.div
+          variants={item}
+          initial={{ x: -10, opacity: 0 }}
+          animate="show"
+          className="sm:w-1/2 lg:w-1/3 m-4 self-center  ">
           <div>
             <h1 className="text-xl mb-2 ">Skills</h1>
             <p>{skills.join(" , ")}</p>
@@ -16,7 +28,11 @@ const Skills = () => {
             <p>{list.join(" , ")}</p>
           </div>
         </motion.div>
-        <motion.div className="sm:w-1/2 lg:w-1/3 m-4 self-center text-end justify-self-end ">
+        <motion.div
+          variants={item}
+          initial={{ x: 10, opacity: 0 }}
+          animate="show"
+          className="sm:w-1/2 lg:w-1/3 m-4 self-center text-end justify-self-end ">
           <div>
             <h1 className="text-xl mb-2 ">Skills</h1>
             <p>{skills.join(" , ")}</p>
