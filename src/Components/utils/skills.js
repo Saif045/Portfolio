2509821faab7e66aeb536f1e9c81f5de63bad704
Saif045 <saif.x45@gmail.com>
2023-01-12 05:57:39 +0200

    final

--- src/Components/utils/skills.js
+++ src/Components/utils/skills.js
@@ -3,14 +3,18 @@ export const skills = [
   "HTML",
   "CSS",
   "Javascript",
-  "TailwindCss",
+  "TailwindCSS",
   "Framer Motion",
   "Git",
   "Redux-Toolkit",
+  "Firebase",
+  "Axios",
+  "MUI",
+  "SCSS",
   "and REST APIs",
 ];
 export const list = ["Next js", "Ts", "SSR"];
 
 export const secondSkills = `I'm quiet confident, naturally curious, and perpetually working on
 improving my chops one design problem at a time. I guarantee a
-commitment during work on your project.`
+commitment during work on your project.`;
